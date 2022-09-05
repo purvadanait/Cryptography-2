6f22fa655fe99e43561fa1750116198d37c65db2
Purva <danaitpurva2001@gmail.com>
2022-09-05 15:25:30 +0530

    initial commit

--- app/src/main/java/com/example/voice/MainActivity.java
+++ app/src/main/java/com/example/voice/MainActivity.java
@@ -2,9 +2,23 @@ package com.example.voice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button btnText;
+    Button btnStop;
+    MediaPlayer player = new MediaPlayer();
+    File file;
+    int REQUEST_WRITE_PERMISSION=1;
+    TextToSpeech textToSpeech;
+    String encry=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
